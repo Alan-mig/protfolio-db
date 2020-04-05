fdd943a90d221f32a27f64a3d89b5ff75976bde2
alanm <alitoalan@gmail.com>
2020-04-05 22:41:36 +0000

    All file update

--- protfolio/urls.py
+++ protfolio/urls.py
@@ -11,8 +11,7 @@ import blog.urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home', jobs.views.home,name='home'),
+    path('', jobs.views.home, name='home'),
     path('blog/', include('blog.urls')),
-    path('<int:blog_id>/',views.detail,name='detail'),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
